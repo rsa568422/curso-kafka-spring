5bbf6d8f9e0c0694aa1daaa19e3053a478a72db8
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-09-05 01:46:31 +0200

    38. Callbacks asíncronas con Spring
    Se agrega un callback a nuestro productor que mostrará el mensaje "Message sent:..." una vez enviado el mensaje, o el mensaje "Error sending message:..." en caso de error.

--- src/main/java/com/devs4j/kafka/CursoKafkaSpringApplication.java
+++ src/main/java/com/devs4j/kafka/CursoKafkaSpringApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @SpringBootApplication
 public class CursoKafkaSpringApplication implements CommandLineRunner {
@@ -28,7 +32,25 @@ public class CursoKafkaSpringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		kafkaTemplate.send("devs4j-topic", "Sample message");
+		ListenableFuture<SendResult<String, String>> future = kafkaTemplate
+				.send("devs4j-topic", "Sample message");
+		future.addCallback(new KafkaSendCallback<>() {
+
+			@Override
+			public void onSuccess(SendResult<String, String> result) {
+				log.info("Message sent: {}", result.getRecordMetadata().offset());
+			}
+
+			@Override
+			public void onFailure(Throwable ex) {
+				log.error("Error sending message: ", ex);
+			}
+
+			@Override
+			public void onFailure(KafkaProducerException ex) {
+				log.error("Error sending message: ", ex);
+			}
+		});
 	}
 
 }
