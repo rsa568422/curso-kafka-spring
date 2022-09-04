629eacb2b334753573ca523637179f43ed0e50ee
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-09-05 01:09:41 +0200

    35. Utilizando un @KafkaListener
    Se realiza la implementación en spring de un consumidor para kafka mediante un método anotado como @KafkaListener en la clase de configuración CursoKafkaSpringApplication.

--- src/main/java/com/devs4j/kafka/CursoKafkaSpringApplication.java
+++ src/main/java/com/devs4j/kafka/CursoKafkaSpringApplication.java
@@ -1,10 +1,20 @@
 package com.devs4j.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class CursoKafkaSpringApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(CursoKafkaSpringApplication.class);
+
+	@KafkaListener(topics = "devs4j-topic", groupId = "devs4j-group")
+	public void listen(String message) {
+		log.info("Message received: {}", message);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoKafkaSpringApplication.class, args);
