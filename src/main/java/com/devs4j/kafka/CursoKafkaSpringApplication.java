ac6185dfc95c690633384fbb17941ef11a489e8f
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-09-04 23:25:40 +0200

    33. Creando un proyecto de Spring y Kafka

---
+++ src/main/java/com/devs4j/kafka/CursoKafkaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.devs4j.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoKafkaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoKafkaSpringApplication.class, args);
+	}
+
+}
