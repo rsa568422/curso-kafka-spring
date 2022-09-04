118743760856fcd9d14cbcb896740415dc2fea22
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-09-05 01:26:31 +0200

    37. Utilizando un producer con Spring framework
    Se agrega a la clase principal CursoKafkaSpringApplication el atributo kafkaTemplate que se iniciará automáticamente con @Autowired y se implementará la interfaz CommandLineRunner junto a su método run, el cual enviará un mensaje de prueba mediante kafkaTemplate.

--- src/main/java/com/devs4j/kafka/CursoKafkaSpringApplication.java
+++ src/main/java/com/devs4j/kafka/CursoKafkaSpringApplication.java
@@ -2,12 +2,18 @@ package com.devs4j.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class CursoKafkaSpringApplication {
+public class CursoKafkaSpringApplication implements CommandLineRunner {
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
 
 	private static final Logger log = LoggerFactory.getLogger(CursoKafkaSpringApplication.class);
 
@@ -18,6 +24,11 @@ public class CursoKafkaSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoKafkaSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		kafkaTemplate.send("devs4j-topic", "Sample message");
 	}
 
 }
