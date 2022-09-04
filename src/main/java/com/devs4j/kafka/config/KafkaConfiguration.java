4eaeec75c39504458e8ed05e6dd28d6e7d6a5c90
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2022-09-05 01:20:17 +0200

    36. Configurando un producer con Spring framework
    Se agregan los métodos producerProperties y kafkaTemplate a la clase de configuración KafkaConfiguration, con la configuración de nuestro productor.

--- src/main/java/com/devs4j/kafka/config/KafkaConfiguration.java
+++ src/main/java/com/devs4j/kafka/config/KafkaConfiguration.java
@@ -1,18 +1,30 @@
 package com.devs4j.kafka.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfiguration {
+
+    public Map<String, Object> producerProperties() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return properties;
+    }
 
     public Map<String, Object> consumerProperties() {
         Map<String, Object> properties = new HashMap<>();
@@ -21,6 +33,12 @@ public class KafkaConfiguration {
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return properties;
+    }
+
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        DefaultKafkaProducerFactory<String, String> producerFactory =
+                new DefaultKafkaProducerFactory<>(producerProperties());
+        return new KafkaTemplate<>(producerFactory);
     }
 
     @Bean
